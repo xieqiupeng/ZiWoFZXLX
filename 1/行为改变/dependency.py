9466a9fd6d47c5971ef4b398e0150a76dc6df795
xieqiupeng <xieqiupeng@gmail.com>
2020-02-14 06:07:14 +0800

    update

---
+++ 1/行为改变/dependency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Enum/Enum-Lesson/职业认同  $Path/5/人生地图/2/成年早期/职业认同
+ln -sf /Volumes/Project/Enum/Enum-Lesson/爱的能力  $Path/5/人生地图/2/成年早期/爱的能力
+ln -sf /Volumes/Project/Enum/Enum-Lesson/心理X光  $Path/1/行为改变/4/心理X光
+ln -sf /Volumes/Project/Enum/Enum-Lesson/高质量关系  $Path/3/高质量关系
+
+# Root
+Path=/Volumes/Project/Enum/Enum-Subject/陈海贤-自我发展心理学
+ln -sf /Volumes/Project/Enum/Enum-Lesson/高质量关系  $Path/3/高质量关系
+
+
+
---
+++ dependency.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Enum/Enum-Lesson/创造新经验  $Path/1/行为改变/2/创造新经验
+ln -sf /Volumes/Project/Enum/Enum-Lesson/心理X光  $Path/1/行为改变/4/心理X光
+
+ln -sf /Volumes/Project/Enum/Enum-Lesson/思维进化  $Path/2/思维进化
+ln -sf /Volumes/Project/Enum/Enum-Lesson/高质量关系  $Path/3/高质量关系
+
+ln -sf /Volumes/Project/Enum/Enum-Lesson/职业认同  $Path/5/人生地图/2/成年早期/职业认同
+ln -sf /Volumes/Project/Enum/Enum-Lesson/爱的能力  $Path/5/人生地图/2/成年早期/爱的能力
+
+# Root
+Path=/Volumes/Project/Enum/Enum-Subject/陈海贤-自我发展心理学
+ln -sf /Volumes/Project/Enum/Enum-Lesson/思维进化  $Path/2/思维进化
+
